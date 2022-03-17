e6794bc103611dea718f6d675eb74b1fb0cf1967
rinucherian2000 <96276005+rinucherian2000@users.noreply.github.com>
2022-03-17 13:24:29 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,25 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 from users.models import User
-
 
 # Create your models here.
 
 
-class CompanyProfile(models.Model):
-    User=models.OneToOneField(User,on_delete=models.CASCADE,null=True)
-    company_name=models.CharField(max_length=120)
-    services=models.CharField(max_length=200)
-    address=models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.company_name
-
-
-class Jobs(models.Model):
-    company=models.ForeignKey(CompanyProfile,on_delete=models.CASCADE,null=True)
-    post=models.CharField(max_length=50)
-    experience=models.CharField(max_length=50)
-    skills=models.CharField(max_length=120)
-    description=models.CharField(max_length=150)
-    posted_date=models.DateField(auto_now_add=True, null=True)
-    last_date=models.DateField()
+class Candidate(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    candidate_name = models.CharField(max_length=100)
+    cv = models.ImageField(upload_to="images")
+    qualification = models.CharField(max_length=200)
+    skills = models.CharField(max_length=200)
+    experience = models.CharField( max_length=200)
